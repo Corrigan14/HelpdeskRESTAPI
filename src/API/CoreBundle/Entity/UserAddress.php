b5c80286ac9c27804bb9e5905c38afc0a58f9d78
LanSystems <mbabinska@bitbucket.org>
2016-10-19 13:41:11 +0200

    user_data, user_address entities

---
+++ src/API/CoreBundle/Entity/UserAddress.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace API\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserAddress
+ *
+ * @ORM\Table(name="user_address")
+ * @ORM\Entity(repositoryClass="API\CoreBundle\Repository\UserAddressRepository")
+ */
+class UserAddress
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zip", type="string", length=255, nullable=true)
+     */
+    private $zip;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     *
+     * @return UserAddress
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return UserAddress
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set zip
+     *
+     * @param string $zip
+     *
+     * @return UserAddress
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    /**
+     * Get zip
+     *
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return UserAddress
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
+
---
+++ src/API/CoreBundle/Entity/UserData.php
@@ -0,0 +1,314 @@
+<?php
+
+namespace API\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserData
+ *
+ * @ORM\Table(name="user_data")
+ * @ORM\Entity(repositoryClass="API\CoreBundle\Repository\UserDataRepository")
+ */
+class UserData
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255, nullable=true)
+     */
+    private $surname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titleBefore", type="string", length=45, nullable=true)
+     */
+    private $titleBefore;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titleAfter", type="string", length=45, nullable=true)
+     */
+    private $titleAfter;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="function", type="string", length=255, nullable=true)
+     */
+    private $function;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mobile", type="string", length=60, nullable=true)
+     */
+    private $mobile;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tel", type="string", length=60, nullable=true)
+     */
+    private $tel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fax", type="string", length=60, nullable=true)
+     */
+    private $fax;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="signature", type="text", nullable=true)
+     */
+    private $signature;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return UserData
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return UserData
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set titleBefore
+     *
+     * @param string $titleBefore
+     *
+     * @return UserData
+     */
+    public function setTitleBefore($titleBefore)
+    {
+        $this->titleBefore = $titleBefore;
+
+        return $this;
+    }
+
+    /**
+     * Get titleBefore
+     *
+     * @return string
+     */
+    public function getTitleBefore()
+    {
+        return $this->titleBefore;
+    }
+
+    /**
+     * Set titleAfter
+     *
+     * @param string $titleAfter
+     *
+     * @return UserData
+     */
+    public function setTitleAfter($titleAfter)
+    {
+        $this->titleAfter = $titleAfter;
+
+        return $this;
+    }
+
+    /**
+     * Get titleAfter
+     *
+     * @return string
+     */
+    public function getTitleAfter()
+    {
+        return $this->titleAfter;
+    }
+
+    /**
+     * Set function
+     *
+     * @param string $function
+     *
+     * @return UserData
+     */
+    public function setFunction($function)
+    {
+        $this->function = $function;
+
+        return $this;
+    }
+
+    /**
+     * Get function
+     *
+     * @return string
+     */
+    public function getFunction()
+    {
+        return $this->function;
+    }
+
+    /**
+     * Set mobile
+     *
+     * @param string $mobile
+     *
+     * @return UserData
+     */
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    /**
+     * Get mobile
+     *
+     * @return string
+     */
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     *
+     * @return UserData
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set fax
+     *
+     * @param string $fax
+     *
+     * @return UserData
+     */
+    public function setFax($fax)
+    {
+        $this->fax = $fax;
+
+        return $this;
+    }
+
+    /**
+     * Get fax
+     *
+     * @return string
+     */
+    public function getFax()
+    {
+        return $this->fax;
+    }
+
+    /**
+     * Set signature
+     *
+     * @param string $signature
+     *
+     * @return UserData
+     */
+    public function setSignature($signature)
+    {
+        $this->signature = $signature;
+
+        return $this;
+    }
+
+    /**
+     * Get signature
+     *
+     * @return string
+     */
+    public function getSignature()
+    {
+        return $this->signature;
+    }
+}
+

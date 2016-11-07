8343ad3f11d0932ddf3eaefde73f742fc29e9cf6
LanSystems <mbabinska@bitbucket.org>
2016-11-07 13:57:41 +0100

    ApiTest Case, Controller test interface - uprava ako na tag vetve

--- src/API/CoreBundle/Tests/Controller/ApiTestCase.php
+++ src/API/CoreBundle/Tests/Controller/ApiTestCase.php
@@ -1,23 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 11/7/16
- * Time: 11:34 AM
- */
 
 namespace API\CoreBundle\Tests\Controller;
 
 use API\CoreBundle\Tests\Traits\LoginTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-abstract class ApiTestCase extends WebTestCase
+abstract class ApiTestCase extends WebTestCase implements ControllerTestInterface
 {
     use LoginTrait;
 
     protected $adminToken;
     protected $userToken;
 
+    protected $client = false;
 
     public function __construct()
     {
@@ -29,4 +24,45 @@ abstract class ApiTestCase extends WebTestCase
         $this->assertNotEquals(false, $this->adminToken);
         $this->assertNotEquals(false, $this->userToken);
     }
+
+    /**
+     * GET LIST - SUCCESS
+     */
+    public function testListSuccess()
+    {
+        $crawler = $this->getClient(true)->request('GET', $this->getBaseUrl(), [], [], ['Authorization' => 'Bearer ' . $this->adminToken, 'HTTP_AUTHORIZATION' => 'Bearer ' . $this->adminToken]);
+        /**
+         * Expect a list of users
+         */
+        //$this->assertEquals(200 , $client->getResponse()->getContent());
+        $this->assertEquals(200, $this->getClient()->getResponse()->getStatusCode());
+    }
+
+    /**
+     * GET LIST - ERRORS
+     */
+    public function testListErrors()
+    {
+        $crawler = $this->getClient(true)->request('GET', $this->getBaseUrl());
+        /**
+         * unauthorized
+         */
+        $this->assertEquals(401, $this->getClient()->getResponse()->getStatusCode());
+        $this->assertContains('Auth header required', $this->getClient()->getResponse()->getContent());
+    }
+
+    /**
+     * @param bool $new
+     * @return bool|\Symfony\Bundle\FrameworkBundle\Client
+     */
+    public function getClient($new = false)
+    {
+        if ($this->client && false === $new) {
+            return $this->client;
+        }
+
+        $this->client = static::createClient();
+        return $this->client;
+    }
+
 }

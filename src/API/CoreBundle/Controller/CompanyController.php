178d1abc272216edf9a0ac8faa979e2303eca5c5
martina <babinskam@aol.com>
2016-11-12 18:47:16 +0100

    empty controller

---
+++ src/API/CoreBundle/Controller/CompanyController.php
@@ -0,0 +1,294 @@
+<?php
+
+namespace API\CoreBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class CompanyController
+ *
+ * @package API\CoreBundle\Controller
+ */
+class CompanyController extends ApiBaseController  implements ControllerInterface
+{
+    /**
+     *  ### Response - option if we have pagination ###
+     *     {
+     *       "data":
+     *       [
+     *          {
+     *            "id": "1",
+     *          }
+     *       ],
+     *       "_links":
+     *       {
+     *           "self": "/entity?page=1",
+     *           "first": "/entity?page=1",
+     *           "prev": false,
+     *           "next": "/entity?page=2",
+     *            "last": "/entity?page=3"
+     *       },
+     *       "total": 22,
+     *       "page": 1,
+     *       "numberOfPages": 3
+     *     }
+     *
+     *
+     * @ApiDoc(
+     *  description="Returns a list of Entities (GET)",
+     *  filters={
+     *     {
+     *       "name"="page",
+     *       "description"="Pagination, limit is set to 10 records"
+     *     }
+     *  },
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  statusCodes={
+     *      200 ="The request has succeeded",
+     *      401 ="Unauthorized request",
+     *  }
+     * )
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function listAction(Request $request)
+    {
+        // TODO: Implement listAction() method.
+    }
+
+    /**
+     *  ### Response ###
+     *      {
+     *        "data":
+     *        {
+     *           "id": "2",
+     *        },
+     *        "_links":
+     *        {
+     *           "put": "/api/v1/entityName/id",
+     *           "patch": "/api/v1/entityName/id",
+     *           "delete": "/api/v1/entityName/id"
+     *         }
+     *      }
+     *
+     * @ApiDoc(
+     *  description="Returns an Entity (GET)",
+     *  requirements={
+     *     {
+     *       "name"="id",
+     *       "dataType"="integer",
+     *       "requirement"="\d+",
+     *       "description"="The id of processed object"
+     *     }
+     *  },
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  output="API\CoreBundle\Entity\...entityName",
+     *  statusCodes={
+     *      200 ="The request has succeeded",
+     *      401 ="Unauthorized request",
+     *      404 ="Not found Entity",
+     *  }
+     * )
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function getAction(int $id)
+    {
+        // TODO: Implement getAction() method.
+    }
+
+    /**
+     * ### Response ###
+     *      {
+     *        "data":
+     *        {
+     *           "id": "2",
+     *        },
+     *        "_links":
+     *        {
+     *           "put": "/api/v1/entityName/2",
+     *           "patch": "/api/v1/entityName/2",
+     *           "delete": "/api/v1/entityName/2"
+     *         }
+     *      }
+     *
+     * @ApiDoc(
+     *  resource = true,
+     *  description="Create a new Entity (POST)",
+     *  input={"class"="API\CoreBundle\Entity\...entityName"},
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  output={"class"="API\CoreBundle\Entity\...entityName"},
+     *  statusCodes={
+     *      201 ="The entity was successfully created",
+     *      401 ="Unauthorized request",
+     *      403 ="Access denied",
+     *      409 ="Invalid parameters",
+     *  }
+     * )
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function createAction(Request $request)
+    {
+        // TODO: Implement createAction() method.
+    }
+
+    /**
+     * ### Response ###
+     *      {
+     *        "data":
+     *        {
+     *           "id": "2",
+     *        },
+     *        "_links":
+     *        {
+     *           "put": "/api/v1/entityName/2",
+     *           "patch": "/api/v1/entityName/2",
+     *           "delete": "/api/v1/entityName/2"
+     *         }
+     *      }
+     *
+     * @ApiDoc(
+     *  description="Update the Entity (PUT)",
+     *  requirements={
+     *     {
+     *       "name"="id",
+     *       "dataType"="integer",
+     *       "requirement"="\d+",
+     *       "description"="The id of processed object"
+     *     }
+     *  },
+     *  input={"class"="API\CoreBundle\Entity\...entityName"},
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  output={"class"="API\CoreBundle\Entity\...entityName"},
+     *  statusCodes={
+     *      200 ="The request has succeeded",
+     *      401 ="Unauthorized request",
+     *      403 ="Access denied",
+     *      404 ="Not found Entity",
+     *      409 ="Invalid parameters",
+     *  }
+     * )
+     *
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateAction(int $id, Request $request)
+    {
+        // TODO: Implement updateAction() method.
+    }
+
+    /**
+     * ### Response ###
+     *      {
+     *        "data":
+     *        {
+     *           "id": "2",
+     *        },
+     *        "_links":
+     *        {
+     *           "put": "/api/v1/entityName/2",
+     *           "patch": "/api/v1/entityName/2",
+     *           "delete": "/api/v1/entityName/2"
+     *         }
+     *      }
+     *
+     * @ApiDoc(
+     *  description="Partially update the Entity (PATCH)",
+     *  requirements={
+     *     {
+     *       "name"="id",
+     *       "dataType"="integer",
+     *       "requirement"="\d+",
+     *       "description"="The id of processed object"
+     *     }
+     *  },
+     *  input={"class"="API\CoreBundle\Entity\...entityName"},
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  output={"class"="API\CoreBundle\Entity\...entityName"},
+     *  statusCodes={
+     *      200 ="The request has succeeded",
+     *      401 ="Unauthorized request",
+     *      403 ="Access denied",
+     *      404 ="Not found Entity",
+     *      409 ="Invalid parameters",
+     *  }
+     * )
+     *
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updatePartialAction(int $id, Request $request)
+    {
+        // TODO: Implement updatePartialAction() method.
+    }
+
+    /**
+     * @ApiDoc(
+     *  description="Delete Entity (DELETE)",
+     *  requirements={
+     *     {
+     *       "name"="id",
+     *       "dataType"="integer",
+     *       "requirement"="\d+",
+     *       "description"="The id of processed object"
+     *     }
+     *  },
+     *  headers={
+     *     {
+     *       "name"="Authorization",
+     *       "required"=true,
+     *       "description"="Bearer {JWT Token}"
+     *     }
+     *  },
+     *  statusCodes={
+     *      204 ="The Entity was successfully deleted",
+     *      401 ="Unauthorized request",
+     *      404 ="Not found Tag",
+     *  })
+     *
+     * @param int $id
+     *
+     * @return JsonResponse
+     */
+    public function deleteAction(int $id)
+    {
+        // TODO: Implement deleteAction() method.
+    }
+}
---
+++ src/API/CoreBundle/Tests/Controller/CompanyControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace API\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CompanyControllerTest extends WebTestCase
+{
+}

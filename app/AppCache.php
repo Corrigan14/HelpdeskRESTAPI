2a0de4a474e3f4bf58c44964de47bf9b6246a659
jurinko <j_chmelicek@yahoo.com>
2016-11-15 19:07:33 +0100

    --no-warmup!

--- app/AppCache.php
+++ app/AppCache.php
@@ -1,32 +1,41 @@
 <?php
 
+require_once __DIR__ . '/AppKernel.php';
+
+use FOS\HttpCache\SymfonyCache\CacheInvalidationInterface;
 use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
 use FOS\HttpCache\SymfonyCache\PurgeSubscriber;
 use FOS\HttpCache\SymfonyCache\RefreshSubscriber;
 use FOS\HttpCache\SymfonyCache\UserContextSubscriber;
-use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpKernel\HttpCache\HttpCache;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 
-class AppCache extends EventDispatchingHttpCache
+class AppCache extends EventDispatchingHttpCache implements CacheInvalidationInterface
 {
+//    use EventDispatchingHttpCache;
 
     /**
-     * http://foshttpcache.readthedocs.io/en/stable/symfony-cache-configuration.html
      *
      * Overwrite constructor to register event subscribers for FOSHttpCache.
      *
-     * @throws \RuntimeException
+     * @param HttpKernelInterface $kernel
+     * @param null $cacheDir
      */
     public function __construct(HttpKernelInterface $kernel, $cacheDir = null)
     {
-        if(null === $cacheDir){
+
+        /* @var AppKernel $kernel */
+        if (null === $cacheDir) {
             $cacheDir = new \Symfony\Component\HttpKernel\HttpCache\Store($kernel->getCacheDir());
         }
         parent::__construct($kernel, $cacheDir);
-        $this->addSubscriber(new UserContextSubscriber());
+//        $this->addSubscriber(new UserContextSubscriber());
         $this->addSubscriber(new PurgeSubscriber());
-        $this->addSubscriber(new RefreshSubscriber());
+//        $this->addSubscriber(new RefreshSubscriber());
     }
+
+
 
 }

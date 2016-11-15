9f6523c879c2a1a728b36b63f74eabc1c5bfb21a
jurinko <j_chmelicek@yahoo.com>
2016-11-15 12:51:45 +0100

    rozbabrany config

--- app/AppCache.php
+++ app/AppCache.php
@@ -1,7 +1,28 @@
 <?php
 
+use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
+use FOS\HttpCache\SymfonyCache\PurgeSubscriber;
+use FOS\HttpCache\SymfonyCache\RefreshSubscriber;
+use FOS\HttpCache\SymfonyCache\UserContextSubscriber;
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
 
 class AppCache extends HttpCache
+//class AppCache extends EventDispatchingHttpCache
 {
+
+//    /**
+//     * http://foshttpcache.readthedocs.io/en/stable/symfony-cache-configuration.html
+//     *
+//     * Overwrite constructor to register event subscribers for FOSHttpCache.
+//     */
+//    public function __construct(HttpKernelInterface $kernel, $cacheDir = null)
+//    {
+//        parent::__construct($kernel, $cacheDir);
+////        $this->addSubscriber(new UserContextSubscriber());
+//        $this->addSubscriber(new PurgeSubscriber());
+////        $this->addSubscriber(new RefreshSubscriber());
+//    }
+
 }

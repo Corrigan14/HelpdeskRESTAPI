5531e3631209a3ca0f2a8bb01aec5e44d4e36279
martina <babinskam@aol.com>
2016-11-16 16:14:22 +0100

    entity, fixtures

---
+++ src/API/TaskBundle/DataFixtures/ORM/CompanyAttributeFixture.php
@@ -0,0 +1,65 @@
+<?php
+namespace API\TaskBundle\DataFixtures\ORM;
+
+use API\TaskBundle\Entity\CompanyAttribute;
+use API\TaskBundle\Services\VariableHelper;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class CompanyAttributeFixture
+ *
+ * @package API\TaskBundle\DataFixtures\ORM
+ */
+class CompanyAttributeFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $ca = new CompanyAttribute();
+        $ca->setTitle('input company additional attribute');
+        $ca->setType(VariableHelper::INPUT);
+        $manager->persist($ca);
+
+        $ca = new CompanyAttribute();
+        $ca->setTitle('select company additional attribute');
+        $ca->setType(VariableHelper::SIMPLE_SELECT);
+        $options = [
+            'select1' => 'select1',
+            'select2' => 'select2',
+            'select3' => 'select3',
+        ];
+        $ca->setOptions(serialize($options));
+        $manager->persist($ca);
+
+        $ca = new CompanyAttribute();
+        $ca->setTitle('integer number company additional attribute');
+        $ca->setType(VariableHelper::INTEGER_NUMBER);
+        $manager->persist($ca);
+
+        $manager->flush();
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 4;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        // TODO: Implement setContainer() method.
+    }
+}
---
+++ src/API/TaskBundle/Entity/CompanyAttribute.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace API\TaskBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use JMS\Serializer\Annotation as Serializer;
+use JMS\Serializer\Annotation\ReadOnly;
+
+/**
+ * CompanyAttribute
+ *
+ * @ORM\Table(name="company_attribute")
+ * @ORM\Entity(repositoryClass="API\TaskBundle\Repository\CompanyAttributeRepository")
+ * @UniqueEntity("title")
+ */
+class CompanyAttribute implements \Serializable
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ReadOnly()
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     * @Assert\NotBlank(message="Title of attribute is required")
+     * @Assert\Type("string")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     * @Assert\NotBlank(message="Type of attribute is required")
+     * @Assert\Type("string")
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="options", type="text", nullable=true)
+     */
+    private $options;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_active", type="boolean", options={"default":1})
+     * @ReadOnly()
+     */
+    private $is_active = true;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return CompanyAttribute
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return CompanyAttribute
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set options
+     *
+     * @param string $options
+     *
+     * @return CompanyAttribute
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * Get options
+     *
+     * @return string
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return CompanyAttribute
+     */
+    public function setIsActive($isActive)
+    {
+        $this->is_active = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->is_active;
+    }
+
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        // TODO: Implement serialize() method.
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        // TODO: Implement unserialize() method.
+    }
+}
+

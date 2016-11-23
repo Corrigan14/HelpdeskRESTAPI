1afb9a3bcaa428187e20e8985afa127f96690871
LanSystems <mbabinska@bitbucket.org>
2016-11-23 13:32:34 +0100

    Rozsirenie dompany controllera na pridavanie company data

--- src/API/TaskBundle/Controller/CompanyController.php
+++ src/API/TaskBundle/Controller/CompanyController.php
@@ -2,55 +2,53 @@
 
 namespace API\TaskBundle\Controller;
 
+use API\CoreBundle\Entity\Company;
+use API\CoreBundle\Security\VoteOptions;
+use API\TaskBundle\Entity\CompanyAttribute;
+use API\TaskBundle\Entity\CompanyData;
+use Igsem\APIBundle\Controller\ApiBaseController;
+use Igsem\APIBundle\Services\StatusCodesHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * Class CompanyController extend's Base Company Controller in CoreBundle
  *
  * @package API\TaskBundle\Controller
  */
-class CompanyController extends Controller
+class CompanyController extends ApiBaseController
 {
     /**
-     *  ### Response ###
-     *     {
-     *       "data":
-     *       [
-     *          {
-     *            "id": "1",
-     *          }
-     *       ],
-     *       "_links":
-     *       {
-     *           "self": "/task-bundle/company?page=1&company=12&company-attribute=1",
-     *           "first": "/task-bundle/company-data?page=1&company=12&company-attribute=1",
-     *           "prev": false,
-     *           "next": "/task-bundle/company-data?page=2&company=12&company-attribute=1",
-     *            "last": "/task-bundle/company-data?page=3&company=12&company-attribute=1"
-     *       },
-     *       "total": 22,
-     *       "page": 1,
-     *       "numberOfPages": 3
-     *     }
-     *
+     * ### Response ###
+     *      {
+     *        "data":
+     *        {
+     *           "id": "2",
+     *           "title": "Web-Solutions"
+     *           "ico": "1102587"
+     *           "dic": "12587459644"
+     *           "ic_dph": "12587459644"
+     *           "street": "Cesta 125"
+     *           "city": "Bratislava"
+     *           "zip": "02587"
+     *           "country": "SR"
+     *        },
+     *        "_links":
+     *        {
+     *           "put": "/api/v1/task-bundle/company-extend/2",
+     *           "patch": "/api/v1/task-bundle/company-extend/2",
+     *           "delete": "/api/v1/task-bundle/company-extend/2"
+     *         }
+     *      }
      *
      * @ApiDoc(
-     *  description="Returns a list of Entities, this list can be based on Company or on Company Attribute (GET)",
-     *  filters={
-     *     {
-     *       "name"="page",
-     *       "description"="Pagination, limit is set to 10 records"
-     *     },
-     *     {
-     *       "name"="company",
-     *       "description"="Company ID"
-     *     },
-     *     {
-     *       "name"="company-attribute",
-     *       "description"="Company Attribute ID"
-     *     }
-     *  },
+     *  description="Create a new Company Entity with extra Company Data.
+     *  This can be added by attributes: company_data[company_attribute_id] = value",
+     *  Attributes must be defined in the CompanyAttribute
+     *  input={"class"="API\CoreBundle\Entity\Company"},
      *  headers={
      *     {
      *       "name"="Authorization",
@@ -58,10 +56,12 @@ class CompanyController extends Controller
      *       "description"="Bearer {JWT Token}"
      *     }
      *  },
+     *  output={"class"="API\CoreBundle\Entity\Company"},
      *  statusCodes={
-     *      200 ="The request has succeeded",
+     *      201 ="The entity was successfully created",
      *      401 ="Unauthorized request",
-     *      403 ="Access denied"
+     *      403 ="Access denied",
+     *      409 ="Invalid parameters",
      *  }
      * )
      *
@@ -70,6 +70,73 @@ class CompanyController extends Controller
      */
     public function createAction(Request $request)
     {
+        if (!$this->get('company_voter')->isGranted(VoteOptions::CREATE_COMPANY)) {
+            return $this->accessDeniedResponse();
+        }
 
+        $requestData = $request->request->all();
+
+        dump($requestData);
+
+        $company = new Company();
+
+        return $this->updateCompany($company, $requestData, true);
+    }
+
+    /**
+     * @param mixed $company
+     * @param array $requestData
+     * @param bool $create
+     *
+     * @return Response|JsonResponse
+     */
+    private function updateCompany($company, array $requestData, $create = false)
+    {
+        $statusCode = $this->getCreateUpdateStatusCode($create);
+
+        if (null === $company || !$company instanceof Company) {
+            return $this->notFoundResponse();
+        }
+
+        $errors = $this->get('entity_processor')->processEntity($company, $requestData);
+
+        if (false === $errors) {
+            $this->getDoctrine()->getManager()->persist($company);
+            $this->getDoctrine()->getManager()->flush();
+
+            /**
+             * Fill CompanyData Entity if some its parameters were sent
+             */
+            if (isset($requestData['company_data']) && count($requestData['company_data']) > 0) {
+                $companyData = $requestData['company_data'];
+                foreach ($companyData as $key => $value) {
+                    $companyAttribute = $this->getDoctrine()->getRepository('APITaskBundle:CompanyAttribute')->find($key);
+                    if ($companyAttribute instanceof CompanyAttribute) {
+                        $cd = new CompanyData();
+                        $cd->setCompany($company);
+                        $cd->setCompanyAttribute($companyAttribute);
+
+                        $cdErrors = $this->get('entity_processor')->processEntity($cd, ['value' => $value]);
+                        if (false === $cdErrors) {
+                            $this->getDoctrine()->getManager()->persist($cd);
+                            $this->getDoctrine()->getManager()->flush();
+                        } else {
+                            $this->createApiResponse([
+                                'message' => 'The value of company_data with key: ' . $key . ' is invalid',
+                            ], StatusCodesHelper::INVALID_PARAMETERS_CODE);
+                        }
+                    } else {
+                        return $this->createApiResponse([
+                            'message' => 'The key: ' . $key . ' of Company Attribute is not valid (Company Attribute with this ID doesn\'t exist)',
+                        ], StatusCodesHelper::INVALID_PARAMETERS_CODE);
+                    }
+                }
+            }
+
+            $entityResponse = $this->get('api_base.service')->getEntityResponse($company, 'company');
+            return $this->createApiResponse($entityResponse, $statusCode);
+        }
+
+        return $this->invalidParametersResponse();
     }
 }
---
+++ src/Traits/CompanyRepositoryTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 11/23/16
+ * Time: 1:24 PM
+ */
+
+namespace Traits;
+
+
+trait CompanyRepositoryTrait
+{
+
+}

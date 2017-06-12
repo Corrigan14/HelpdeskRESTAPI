cca83929979213081142f18ec9228346ec1bd4b6
jurinko <j_chmelicek@yahoo.com>
2017-06-12 15:27:03 +0200

    fix assigned

--- src/API/TaskBundle/Controller/Task/TaskController.php
+++ src/API/TaskBundle/Controller/Task/TaskController.php
@@ -1460,6 +1460,8 @@ class TaskController extends ApiBaseController
             ], StatusCodesHelper::INVALID_PARAMETERS_CODE);
         }
 
+
+
         if (isset($requestData['requester'])) {
             $requestedUser = $this->getDoctrine()->getRepository('APICoreBundle:User')->find($requestData['requester']);
             if (!$requestedUser instanceof User) {
@@ -1469,9 +1471,12 @@ class TaskController extends ApiBaseController
             }
             $task->setRequestedBy($requestedUser);
         } else {
-            return $this->createApiResponse([
-                'message' => 'The Requester of the task is required!',
-            ], StatusCodesHelper::INVALID_PARAMETERS_CODE);
+
+            $task->setRequestedBy($this->getUser());
+            
+//            return $this->createApiResponse([
+//                'message' => 'The Requester of the task is required!',
+//            ], StatusCodesHelper::INVALID_PARAMETERS_CODE);
         }
 
         if (isset($requestData['important'])) {
@@ -1518,7 +1523,7 @@ class TaskController extends ApiBaseController
         if (isset($requestData['project'])) {
             $project = $this->getDoctrine()->getRepository('APITaskBundle:Project')->find($requestData['project']);
 //>>>>>>> origin/master
-            
+
 
             if (!$project instanceof Project) {
                 return $this->createApiResponse([
@@ -1596,6 +1601,9 @@ class TaskController extends ApiBaseController
 
         // OPTIONAL PARAMETERS - ANOTHER NEW ENTITY IS REQUIRED
         if (isset($requestData['assigned'])) {
+            dump($requestData['assigned']);
+            $requestData['assigned']=json_decode($requestData['assigned'], true);
+            dump($requestData['assigned']);
             // Add new requested users to the task
             foreach ($requestData['assigned'] as $key => $value) {
                 $assignedUserId = $value['userId'];
